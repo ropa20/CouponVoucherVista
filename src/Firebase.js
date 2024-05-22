472d0f096429568514ed3ab02c0e18466f5e367d
Ropa <84048917+ropa20@users.noreply.github.com>
2024-05-22 09:41:01 +0530

    firebase independent

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getToken, getMessaging, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_STORAGE_BUCKET,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY || "YOUR_API_KEY",
+  authDomain:
+    process.env.REACT_APP_AUTH_DOMAIN || "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL:
+    process.env.REACT_APP_DATABASE_URL ||
+    "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: process.env.REACT_APP_PROJECT_ID || "YOUR_PROJECT_ID",
+  storageBucket:
+    process.env.REACT_APP_STORAGE_BUCKET || "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId:
+    process.env.REACT_APP_STORAGE_BUCKET || "YOUR_MESSAGING_SENDER_ID",
+  appId: process.env.REACT_APP_APP_ID || "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
 };
 
 console.log("*** Firebase Config ***", firebaseConfig);
